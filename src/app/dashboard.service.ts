b5119be1297c58ff20aa210143fff2b79ec2c5a9
khalid zarrouki <khzarroukigit@gmail.com>
2021-09-13 17:59:10 +0100

    add dashboard service

--- src/app/dashboard.service.ts
+++ src/app/dashboard.service.ts
@@ -6,7 +6,7 @@ export class DashboardService {
   getTeammembers():any[]{
     var TeamMemberSummary=[
       {Region:"West",TeamMemberCount:20,TemporaryUnavailableMember:16},
-      {Region:"East",TeamMemberCount:16,TemporaryUnavailableMember:10},
+      {Region:"East",TeamMemberCount:16,TemporaryUnavailableMember:20},
       {Region:"North",TeamMemberCount:50,TemporaryUnavailableMember:14},
       {Region:"South",TeamMemberCount:100,TemporaryUnavailableMember:40}
     ];
